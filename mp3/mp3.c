c9d624d1e0608a6e31d95d438dfb76ce20113573
Stephanie Tancs <stancs2@illinois.edu>
2020-02-20 18:27:34 -0600

    mp3

--- mp3/mp3.c
+++ mp3/mp3.c
@@ -1,14 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* This program takes an integer input from the user and outputs the row of Pascal's Triangle that corresponds to that number, defining 0 as the first row.
+partners: stancs2, anudeep2, asanag2
+*/
+
 int main()
 {
-  int row;
+  int row; //defined by user
+  long num = 1; //final answer
 
-  printf("Enter a row index: ");
-  scanf("%d",&row);
+  printf("Enter a row index: "); //prompts user
+  scanf("%d",&row); //stores response
 
-  // Write your code here
+  printf("1 "); //first line is always the same
+
+  for (int n = 1; n <= row; n++){
+    float x = 1; //defines the number for the current row and position
+    for (int k = 1; k <= n; k++){
+      int part = 1-k; //defines part of the numerator of the formula for ease of read/debug
+      float numerator = ((float) row) +((float)part); //defines the rest of the numerator of the formula
+      float temp  = numerator /k; //defines the denominator of the formula
+      x = temp * x; //mulitplies with previous loop values
+    }
+    num = x; //makes x an long value
+    printf("%ld",num); //prints the value
+    printf(" "); //prints a space
+  }
 
   return 0;
 }
