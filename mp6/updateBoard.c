2ee855b2626caa0d5de37a96e60f29113f56c10a
Stephanie Tancs <stancs2@illinois.edu>
2020-03-11 17:13:07 -0500

    mp6

--- mp6/updateBoard.c
+++ mp6/updateBoard.c
@@ -13,6 +13,20 @@
  */
 
 int countLiveNeighbor(int* board, int boardRowSize, int boardColSize, int row, int col){
+  //location -> #ofcols*row+col
+  int liveNeighbors = 0,i,j;
+
+  //loop through every section of 3 elements and report live neighbors within the area
+  for(i = row-1; i<=row+1; i++){
+    for(j = col-1; j<=col+1; j++){
+      if(i>=0 && j>=0 && i<boardRowSize  && j<boardColSize)
+	if(board[i*boardColSize+j]==1)
+	  liveNeighbors++;
+    }
+  }
+  if(board[row*boardColSize+col]==1)
+    liveNeighbors--;
+  return liveNeighbors;
 }
 /*
  * Update the game board to the next step.
@@ -24,6 +38,33 @@ int countLiveNeighbor(int* board, int boardRowSize, int boardColSize, int row, i
  * Output: board is updated with new values for next step.
  */
 void updateBoard(int* board, int boardRowSize, int boardColSize) {
+  //initialize all variables and a temporary board
+  int boardSize = boardRowSize*boardColSize;
+  int tempBoard[boardSize];
+  int i,j,copy,liveNeighbors;
+
+  //loops through every element, counts each elements live neighbors
+  //if an element is equal to 0 and has 3 live neighbors, it is now 1
+  //if an element is equal to 1 and has less than 2 or more than 3 live neighbors, it is now 0
+  for(i=0; i<boardRowSize; i++){
+    for(j=0; j<boardColSize; j++){
+      liveNeighbors = countLiveNeighbor(board,boardRowSize,boardColSize,i,j);
+      if(board[i*boardColSize+j]==0){
+	if(liveNeighbors==3)
+	  tempBoard[i*boardColSize+j]=1;
+	else
+	  tempBoard[i*boardColSize+j]=0;
+      }
+      if(board[i*boardColSize+j]==1){
+	if(liveNeighbors<2||liveNeighbors>3)
+	  tempBoard[i*boardColSize+j]=0;
+	else
+	  tempBoard[i*boardColSize+j]=1;
+      }
+    }
+  }
+  for(copy=0; copy<boardSize; copy++)
+    board[copy]=tempBoard[copy];
 }
 
 /*
@@ -38,6 +79,26 @@ void updateBoard(int* board, int boardRowSize, int boardColSize) {
  * return 0 if the alive cells change for the next step.
  */ 
 int aliveStable(int* board, int boardRowSize, int boardColSize){
+  //initialize all variables and temporary array
+  int boardSize = boardRowSize*boardColSize;
+  int tempBoard[boardSize];
+  int copy, update;
+
+  //copy all elements into a temporary board and use this to update the game board
+  for(copy=0; copy<boardSize; copy++){
+    tempBoard[copy]=board[copy];
+  }
+  
+  updateBoard(tempBoard, boardRowSize, boardColSize);
+
+  //if there is a change in the value of the cells, return 0, else return 1
+  for(update=0; update<boardSize; update++){
+    if(tempBoard[update]!=board[update])
+       return 0;
+  }
+  
+  return 1;
+  
 }
 
 				
