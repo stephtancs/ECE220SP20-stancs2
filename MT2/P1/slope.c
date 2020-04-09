a32eaa07ae0ed13f34fa839a7bff4c38954a2db2
Huizi Hu <huizihu2@linux-39.ews.illinois.edu>
2020-04-09 08:55:06 -0500

    Midterm2

---
+++ MT2/P1/slope.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "slope.h"
+
+
+// Return index of the first row that is not a slope; otherwise return -1
+int find_nonslope_row(int array[6][6]) {
+  // Your code starts here
+
+
+}
+
+
+int read_array(char* filename, int array[6][6]){
+  // Your code starts here
+}
+
---
+++ MT2/P3/Search.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "Search.h"
+
+//Your implementation of this function must be recursive
+//return 1 if item found in data; otherwise, return 0
+int Search(int item, int data[ROW][COL], int start_row, int end_row, int start_col, int end_col){
+    //Your code starts here
+
+
+}
+
+
